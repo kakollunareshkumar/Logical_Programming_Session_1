15ee9fa6881bb8da768fd95fda669cbafa49eab5
kakollu naresh kumar <51593016+kakollunareshkumar@users.noreply.github.com>
2021-07-17 19:36:50 +0530

    main
    Added Session_6 programs

---
+++ src/palindrom_or_not/Dummy.java
@@ -0,0 +1,30 @@
+package palindrom_or_not;
+
+public class Dummy {
+    public static void main(String[] args) {
+        //System.out.println(5/2);
+        //1st case
+        int a=1;
+        a=++a;
+
+        int b=2;
+
+        int c=a+b;
+        System.out.println("c value "+c);
+        System.out.println(" a value "+a);
+//2nd case
+        //Googling question?
+        int a1=1;
+        a1++;
+
+        int b1=2;
+
+        int c1=a1+b1;
+
+        System.out.println("c1 value "+c1);
+        System.out.println(" a1 value "+a1);
+        a1=a1+2;
+        System.out.println(" a1 value "+a1);
+
+    }
+}
---
+++ src/palindrom_or_not/String_Palindrom_Or_Not.java
@@ -0,0 +1,34 @@
+package palindrom_or_not;
+
+import java.util.stream.IntStream;
+
+public class String_Palindrom_Or_Not {
+    public static void main(String[] args) {
+
+        String str = "abccba", reverseStr = "";
+
+        int strLength = str.length();
+        //Method 1
+        for (int i = (strLength - 1); i >=0; --i) {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+
+        if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+            System.out.println(str + " is a Palindrome String.");
+        }
+        else {
+            System.out.println(str + " is not a Palindrome String.");
+        }
+
+        //Method 2
+        String reversedString = new StringBuilder(str).reverse().toString();
+
+        System.out.println(" String reverse using string builder "+str.equals(reversedString) +" "+reversedString);
+        //Method 3
+        String tempString = str.replaceAll("\\s+", "").toLowerCase();
+
+//        return IntStream.range(0, tempString.length() / 2)
+//                .noneMatch(i - > tempString.charAt(i) != tempString.charAt(tempString.length() - i - 1));
+
+    }
+}
