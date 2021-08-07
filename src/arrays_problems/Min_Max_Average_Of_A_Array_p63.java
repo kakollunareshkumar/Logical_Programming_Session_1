54d0ec70c043f8aa095321f8bf4591aa03cb441a
kakollu naresh kumar <51593016+kakollunareshkumar@users.noreply.github.com>
2021-08-07 07:48:54 +0530

    main
    Added comments.

--- src/arrays_problems/Min_Max_Average_Of_A_Array_p63.java
+++ src/arrays_problems/Min_Max_Average_Of_A_Array_p63.java
@@ -7,21 +7,22 @@ public class Min_Max_Average_Of_A_Array_p63 {
         int min = 0;
 
         int[] values = {90, -1, 2, 75, 30, 40,10,28,58,25,83};
-
-
         for (int i=0;i< values.length;i++)
         {
+            //Summing for every element
             sum = sum+values[i];
-
+            //if values[i] element is lesser than the minimum element then make min=value[i]
             if(min>values[i])
             {
                 min=values[i];
             }
+            //if values[i] element is greater than the minimum element then make max=value[i]
             if(max<values[i])
             {
                 max=values[i];
             }
         }
+        //Calculate the double value of an array
         double average = (double) sum/values.length;
         System.out.println("Minimum value of an array "+min);
         System.out.println("Maximum value of an array "+max);
